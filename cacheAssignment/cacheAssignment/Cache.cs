c1ba890a487943e2875eae78d351dd00473c75f1
Rajpreet Singh Nayyar <rnayyar@tavisca.com>
2018-09-11 15:24:12 +0530

    cache implemented

---
+++ cacheAssignment/cacheAssignment/Cache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+namespace cacheAssignment
+{
+    class Cache
+    {
+      
+        MemoryCache cache = new MemoryCache("NewCache");
+        public void AddInCache(string key, Object value)
+        {
+            cache.Add(key, value, DateTime.Now.AddSeconds(20));
+        }
+
+        public object GetItemFromCache(string key)
+        {
+            var res = cache[key];
+            return res;
+        }
+
+    }
+}
---
+++ cacheAssignment/cacheAssignment/CacheService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cacheAssignment
+{
+    class CacheService : IProductService
+    {
+
+        private IProductService _proService = new ProductService();
+        private Cache _CacheObj = new Cache();
+
+
+        public Product GetProductById(int id)
+        {
+            
+            Product product = (Product)_CacheObj.GetItemFromCache(Convert.ToString(id));
+            if (product != null)
+            {
+                Console.WriteLine(" Retrieveing From Cache");
+                return product;
+            }
+            else
+            {
+                product = _proService.GetProductById(id);
+                _CacheObj.AddInCache(Convert.ToString(product.Id), product);
+            }
+            
+            return product;
+
+        }
+        public List<Product> GetAllProducts()
+        {
+            List<Product> products = _proService.GetAllProducts();
+ 
+            return products;
+        }
+    }
+}
